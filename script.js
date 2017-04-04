54a999334257e15159869c969f2fe47d416a09d8
Vishal <vishal.koosuri1@marist.edu>
2017-04-04 14:10:51 +0000

    Creates a new notes-area with same styling as the parent

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $('#add').click(function() {
     $(this).before($('<textarea></textarea>')).appendTo('div.container');
+    $('textarea').addClass('notes-box');
+    $('textarea').attr('Placeholder', 'Start typing here...');
   });
 });

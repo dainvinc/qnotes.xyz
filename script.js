5e2458635dcc0ccbe954f82466e536fa17f1b9b4
Vishal <vishal.koosuri1@marist.edu>
2017-07-17 18:05:23 +0000

    Changed the id to a class name

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#add').click(function() {
+  $('.btn-floating').click(function() {
     $(this).before($('<textarea></textarea>')).appendTo('div.container');
     $('textarea').addClass('notes-box');
     $('textarea').attr('Placeholder', 'Start typing here...');
